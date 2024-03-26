6da52b6c787da6e3eff63ed379ad6d0e2c82cbe2
hhejo <litsmainjof@gmail.com>
2024-03-26 21:18:06 +0900

    add: firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Firebase
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+
+// firebase 설정
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app); // auth
+export const firestore = getFirestore(app); // firestore
+// const analytics = getAnalytics(app);
